1b803756a09c990cd840c292bb127b69647a7e05
jack.ross <jack.ross@logicalposition.com>
2021-11-20 19:56:01 -0600

    CSS work, routing work, and homepage content

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import {
+  BrowserRouter,
+  Route
+} from "react-router-dom";
+
 import Nav from './Components/Nav/Nav';
 import About from './Components/About/About';
 import Work from './Components/Work/Work';
@@ -7,14 +12,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <main>
-        <About />
-        <Work />
-        <Contact />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <main>
+          <Route exact path="/"><About /></Route>
+          <Route path="/work"><Work /></Route>
+          <Route path="/contact"><Contact /></Route>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,22 @@ import styles from './About.module.css';
 
 const About = props => {
     return (
-        <h1>About Loaded.</h1>
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <div className={styles.content}>
+                    <h1>JACK ROSS</h1>
+                    <h2>FULL STACK WEB DEVELOPER</h2>
+                    <h3>CHICAGO, IL</h3>
+                    <p>I am an aspiring full stack developer with experience in Python, C# and MEAN. I enjoy working in a team and collaborating with others. I am passionate about creating and am always looking to further advance my skill set. I am looking to join a team of passionate developers whom I can both learn from and contribute to.</p>
+                </div>
+
+                <div className={styles.threeJS} />
+            </div>
+
+            <div className={styles.right}>
+                <p>4 qna</p>
+            </div>
+        </div>
     )
 }
 
