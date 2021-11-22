00f0db934373f39e5b73555d62047cb91adf3397
jack.ross <jack.ross@logicalposition.com>
2021-11-21 22:18:52 -0600

    Some more general CSS work as well as started the 'Contact' page.

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,7 +3,22 @@ import styles from './Contact.module.css';
 
 const Contact = props => {
     return (
-        <h1>Contact Loaded.</h1>
+        <div className={styles.container}>
+            <h1>Contact Me</h1>
+            <form>
+                <strong><label>Name</label></strong>
+                <input type="text" />
+                <strong><label>Email</label></strong>
+                <input type="text" />
+                <strong><label>Phone</label></strong>
+                <input type="text" />
+                <strong><label>How did you hear about me?</label></strong>
+                <input type="text" />
+                <strong><label>Message</label></strong>
+                <input type="textarea" />
+                <input type="submit" />
+            </form>
+        </div>
     )
 }
 
